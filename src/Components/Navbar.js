140a8de4b7ef7278dcbd3b9e29acf561a6d67373
sherksiva <prakashraman441@gmail.com>
2025-11-11 05:45:38 +0530

    For versal build

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Avatar from '@mui/material/Avatar';
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,16 +1,10 @@
 import React from "react";
 import Todo from "./Todo";
 import { useState, useEffect } from "react";
-import { Input, Stack, Button } from "@mui/material";
-import Typography from "@mui/material/Typography";
 import "./todos.css";
 import { auth, db,  } from "../firebase";
 
 function Todos() {
-  const ariaLabel = { "aria-label": "description" };
-
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
   const [photourl, setPhotourl] = useState("");
   const [currentUser, setCurrentUser] = useState ("");
   const [user, setUser] = useState(null);
@@ -27,19 +21,6 @@ function Todos() {
 	   setCurrentUser(auth.currentUser);
            setPhotourl(auth.currentUser.photoURL);
         }
-        db.collection(auth.currentUser.uid)
-          .orderBy("timestamp", "desc")
-          .onSnapshot((snapshot) => {
-            setTodos(
-              snapshot.docs.map((doc) => {
-                return {
-                  id: doc.id,
-                  todoText: doc.data().todoText,
-                  timestamp: doc.data().timestamp,
-                };
-              })
-            );
-          });
       } else {
         // user logged out
 	setCurrentUser("");
@@ -48,23 +29,7 @@ function Todos() {
     });
   }, [user]);
 
-  const addTodo = (e) => {
-    e.preventDefault();
-    db.collection(auth.currentUser.uid).doc(input).set({
-      todoText: input,
-      timestamp: new Date().toISOString(),
-    });
-    setInput("");
-  };
-
-  const deleteTodo = (id) => {
-    try {
-      db.collection(auth.currentUser.uid).doc(id).delete();
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
+ 
   return (
     <div className="todosSection">
 
@@ -74,31 +39,6 @@ function Todos() {
 	<h4>{currentUser.email}</h4>
 	</>
 }
-     {/* <form>
-        <Input
-          name="todoname"
-          className="todoInput"
-          placeholder="Add a todo"
-          inputProps={ariaLabel}
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <Button type="submit" onClick={addTodo} variant="outlined">
-          Add
-        </Button>
-      </form>
-
-      {todos.map((todo) => {
-        return (
-          <Todo
-            key={todo.id}
-            todoText={todo.todoText}
-            timestamp={todo.timestamp}
-            deleteTodo={deleteTodo}
-            todoID={todo.id}
-          />
-        );
-      })} */}
     </div>
   );
 }
